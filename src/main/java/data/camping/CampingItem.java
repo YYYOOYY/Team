0d7a524a323fb9c6739fb0521f88ea0f4fab1b40
YYYOOYY <dideoduf1@gmail.com>
2023-04-27 11:24:14 +0900

    feat: 미완성 웰컴페이지

--- src/main/java/data/camping/CampingItem.java
+++ src/main/java/data/camping/CampingItem.java
@@ -1,6 +1,9 @@
 package data.camping;
 
 public class CampingItem {
+	// 캠핑장 ID
+	String contentId;
+	
 	// 상호
 	String facltNm;
 	
@@ -44,6 +47,7 @@ public class CampingItem {
 	String mapY;
 	
 	// 캠핑장 오는길 부가설명 ex) 울양양고속도로 강촌IC에서 ... 30분 후 도착
+	String direction;
 	
 	// 대표전화
 	String tel;
@@ -69,7 +73,7 @@ public class CampingItem {
 	// 운영기간 ex) 평일 + 주말
 	String operDeCl;
 	
-	// 바베큐 시설정보 ex) 개별, 공동
+	// 바베큐 시설정보 ex) 개별, 공동, 불가
 	String brazierCl;
 	
 	// 캠핑장 시설정보 (고캠핑에 캠핑장 시설정보)
@@ -87,6 +91,79 @@ public class CampingItem {
 	// 캠핑장 태그
 	String themaEnvrnCl;
 
+	// 일반야영장 갯수
+	String gnrlSiteCo;
+	
+	// 자동차야영장 갯수
+	String autoSiteCo;
+	
+	// 글램핑 갯수
+	String glampSiteCo;
+	
+	// 카라반 갯수
+	String caravSiteCo;
+	
+	// 개인 카라반 사이트 갯수
+	String indvdlCaravSiteCo;
+	
+	// 사이트 간격
+	String sitedStnc;
+	
+	// 사이트 1 크기 - 가로
+	String siteMg1Width;
+	
+	// 사이트 1 크기 - 세로
+	String siteMg1Vrticl;
+	
+	// 사이트 1 갯수
+	String siteMg1Co;
+	
+	// 사이트 2 크기 - 가로
+	String siteMg2Width;
+		
+	// 사이트 2 크기 - 세로
+	String siteMg2Vrticl;
+		
+	// 사이트 2 갯수
+	String siteMg2Co;
+		
+	// 사이트 3 크기 - 가로
+	String siteMg3Width;
+	
+	// 사이트 3 크기 - 세로
+	String siteMg3Vrticl;
+		
+	// 사이트 3 갯수
+	String siteMg3Co;
+		
+	// 바닥형태 - 파쇄석
+	String siteBottomCl2;
+	
+	// 바닥형태 - 자갈
+	String siteBottomCl4;
+		
+	// 바닥형태 - 테크
+	String siteBottomCl3;
+	
+	// 바닥형태 - 잔디
+	String siteBottomCl1;
+	
+	// 바닥형태 - 맨흙
+	String siteBottomCl5;
+	
+	// 소화기
+	String extshrCo;
+	
+	// 방화수
+	String frprvtWrppCo;
+	
+	// 방화사
+	String frprvtSandCo;
+	
+	// 화재감시기
+	String fireSensorCo;
+	
+	
 	public String getFacltNm() {
 		return facltNm;
 	}
@@ -197,6 +274,110 @@ public class CampingItem {
 
 	public String getThemaEnvrnCl() {
 		return themaEnvrnCl;
+	}
+
+	public String getContentId() {
+		return contentId;
+	}
+
+	public String getDirection() {
+		return direction;
+	}
+
+	public String getGnrlSiteCo() {
+		return gnrlSiteCo;
+	}
+
+	public String getAutoSiteCo() {
+		return autoSiteCo;
+	}
+
+	public String getGlampSiteCo() {
+		return glampSiteCo;
+	}
+
+	public String getCaravSiteCo() {
+		return caravSiteCo;
+	}
+
+	public String getIndvdlCaravSiteCo() {
+		return indvdlCaravSiteCo;
+	}
+
+	public String getSitedStnc() {
+		return sitedStnc;
+	}
+
+	public String getSiteMg1Width() {
+		return siteMg1Width;
+	}
+
+	public String getSiteMg1Vrticl() {
+		return siteMg1Vrticl;
+	}
+
+	public String getSiteMg1Co() {
+		return siteMg1Co;
+	}
+
+	public String getSiteMg2Width() {
+		return siteMg2Width;
+	}
+
+	public String getSiteMg2Vrticl() {
+		return siteMg2Vrticl;
+	}
+
+	public String getSiteMg2Co() {
+		return siteMg2Co;
+	}
+
+	public String getSiteMg3Width() {
+		return siteMg3Width;
+	}
+
+	public String getSiteMg3Vrticl() {
+		return siteMg3Vrticl;
+	}
+
+	public String getSiteMg3Co() {
+		return siteMg3Co;
+	}
+
+	public String getSiteBottomCl2() {
+		return siteBottomCl2;
+	}
+
+	public String getSiteBottomCl4() {
+		return siteBottomCl4;
+	}
+
+	public String getSiteBottomCl3() {
+		return siteBottomCl3;
+	}
+
+	public String getSiteBottomCl1() {
+		return siteBottomCl1;
+	}
+
+	public String getSiteBottomCl5() {
+		return siteBottomCl5;
+	}
+
+	public String getExtshrCo() {
+		return extshrCo;
+	}
+
+	public String getFrprvtWrppCo() {
+		return frprvtWrppCo;
+	}
+
+	public String getFrprvtSandCo() {
+		return frprvtSandCo;
+	}
+
+	public String getFireSensorCo() {
+		return fireSensorCo;
 	}
 	
 	
